ef3e1981f25bfcee5bb273b46581c2e8b8e9363c
Yes-Kela <vospen06@mail.ru>
2024-08-22 23:17:38 +0400

    packed the structures into an array

--- Square.cpp
+++ Square.cpp
@@ -1,4 +1,4 @@
-// Решатель квадратных уравнений.
+// Решатель квадратных уравнений
 
 #include <TXLib.h>
 #include <stdio.h>
@@ -16,30 +16,45 @@ int compare_double (double first, double second);
 void MainInput (double* cf_a, double* cf_b, double* cf_c);
 void Input (double* cf);
 int EquationSolver (double a, double b, double c, double* x1, double* x2);
-int Linear (double b, double c, double* x1);
+int Linear (double b, double c, double* x1, double* x2);
 int Square (double a, double b, double c, double* x1, double* x2);
 void Output (double x1, double x2, int Roots_count);
 int RunAllTests(TEST data);
 
+#define SHOWTESTS
 int main(void)
 {
-    TEST test1 = {.Test_count = 1, .a = 1, .b = -12, .c = 35, .x1_ideal = 5, .x2_ideal = 7, .Roots_count_ideal = 2};
+    TEST test[] = {
+    {.Test_count = 1, .a = 1,      .b = -12,    .c = 35, .x1_ideal = 5, .x2_ideal = 7, .Roots_count_ideal = 2},
+    {.Test_count = 2, .a = 5,      .b = 9,      .c = -14, .x1_ideal = -2.8, .x2_ideal = 1, .Roots_count_ideal = 2},
+    {.Test_count = 3, .a = 0,      .b = 0,      .c = 0, .x1_ideal = 0, .x2_ideal = 0, .Roots_count_ideal = -1},
+    {.Test_count = 4, .a = 1,      .b = 0,      .c = 5, .x1_ideal = 0, .x2_ideal = 0, .Roots_count_ideal = 0},
+    {.Test_count = 5, .a = 0,      .b = 2,      .c = -6, .x1_ideal = 3, .x2_ideal = 3, .Roots_count_ideal = 1},
+    {.Test_count = 6, .a = 0.0001, .b = 1.2345, .c = 1.2344, .x1_ideal = -12344, .x2_ideal = -1, .Roots_count_ideal = 2},
+    {.Test_count = 7, .a = 0.05,   .b = 0.09,   .c = -0.14, .x1_ideal = -2.8, .x2_ideal = 1, .Roots_count_ideal = 2}};
+
     printf("Эта программа умеет решать квадратное уравнение вида ax^2 + bx + c = 0.\n\n");
-    /*
+
     double cf_a = 0, cf_b = 0, cf_c = 0;
     printf("Введите значения коэффициентов a, b и c в этом порядке.\n");
     MainInput(&cf_a, &cf_b, &cf_c);
     double x1 = 0, x2 = 0;
     int Roots_count = EquationSolver (cf_a, cf_b, cf_c, &x1, &x2);
     Output(x1, x2, Roots_count);
-    */
-    printf("# RunAllTests: Test 1: %d", RunAllTests(test1));
+
+    #ifdef SHOWTESTS
+        for (int i = 0; i < 7; i++)
+        {
+            RunAllTests(test[i]);
+        }
+    #endif
+
     return 0;
 }
 
 int compare_double (double first, double second)
 {
-    const double epsilon = 1e-12;
+    const double epsilon = 1e-5;
     if (fabs(first - second) < epsilon)
         return 1;
     else
@@ -52,7 +67,7 @@ void MainInput (double* cf_a, double* cf_b, double* cf_c)
     Input(cf_a);
     printf("Коэффициент b: ");
     Input(cf_b);
-    printf("Коэффийиент c: ");
+    printf("Коэффициент c: ");
     Input(cf_c);
 }
 
@@ -72,12 +87,12 @@ int EquationSolver (double a, double b, double c, double* x1, double* x2)
     int a_is_0 = compare_double(a, 0);
 
     if (a_is_0)
-        return Linear (b, c, x1);
+        return Linear (b, c, x1, x2);
     else
         return Square (a, b, c, x1, x2);
 }
 
-int Linear (double b, double c, double* x1)
+int Linear (double b, double c, double* x1, double* x2)
 {
     int  b_is_0 = compare_double(b, 0), c_is_0 = compare_double(c, 0);
     if (b_is_0 && c_is_0)
@@ -86,7 +101,7 @@ int Linear (double b, double c, double* x1)
         return 0;
     else
     {
-        * x1 = -c / b;
+        * x1 = * x2 = -c / b;
         return 1;
     }
 }
@@ -100,7 +115,7 @@ int Square (double a, double b, double c, double* x1, double* x2)
         return 0;
     else if (compare_double(discriminant, 0))
     {
-        * x1 = common;
+        * x1 = * x2 = common;
         return 1;
     }
     else
@@ -135,17 +150,25 @@ void Output (double x1, double x2, int Roots_count)
 }
 
 
-// Юнит-тестирование, передаланное:
+// Юнит-тестирование
 
 int RunAllTests (TEST data)
 {
-    double x1, x2;
+    double x1 = 0, x2 = 0;
     int Roots_count = EquationSolver(data.a, data.b, data.c, &x1, &x2);
+
     if (Roots_count != data.Roots_count_ideal || !compare_double(x1, data.x1_ideal) || !compare_double(x2, data.x2_ideal))
-        printf("# ERROR Test %d, a = %lg, b = %lg, c = %lg, Roots_count = %d, x1 = %lg, x2 = %lg\n"
-               "Expected: x1 = %lg, x2 = %lg, Roots_count = %d\n",
+        printf("# ERROR: Test %d: a = %lg, b = %lg, c = %lg, Roots_count = %d, x1 = %lg, x2 = %lg\n"
+               "  Expected: Roots_count = %d, x1 = %lg, x2 = %lg\n",
                 data.Test_count, data.a, data.b, data.c, Roots_count, x1, x2,
-                data.x1_ideal, data.x2_ideal, data.Roots_count_ideal);
+                data.Roots_count_ideal, data.x1_ideal, data.x2_ideal);
+    else
+        printf("# SUCCESS: Test %d: a = %lg, b = %lg, c = %lg, Roots_count = %d, x1 = %lg, x2 = %lg\n",
+                data.Test_count, data.a, data.b, data.c, Roots_count, x1, x2);
+
     return data.Test_count;
 }
 
+
+
+// Нужна функция, которая выводила бы все тесты на экран. Она должна принимать
