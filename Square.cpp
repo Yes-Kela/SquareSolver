218024291f74abe637f820de49c9a81026ddfaa4
Yes-Kela <vospen06@mail.ru>
2024-08-22 09:53:31 +0400

    turned Inputs off the MainInput

--- Square.cpp
+++ Square.cpp
@@ -5,25 +5,24 @@
 #include <math.h>
 
 int compare_double (double first, double second);
-void Input (double* kf);
+void MainInput (double* cf_a, double* cf_b, double* cf_c);
+void Input (double* cf);
 int EquationSolver (double a, double b, double c, double* x1, double* x2);
 int Linear (double b, double c, double* x1);
 int Square (double a, double b, double c, double* x1, double* x2);
 void Output (double x1, double x2, int Roots_count);
-// int RunAllTests(int Test_count, double a, double b, double c, double x1_ideal, double x2_ideal, double Roots_count_ideal);
+int RunAllTests(int Test_count, double a, double b, double c, double x1_ideal, double x2_ideal, int Roots_count_ideal);
 
 int main(void)
 {
     printf("Эта программа умеет решать квадратное уравнение вида ax^2 + bx + c = 0.\n\n");
     double cf_a = 0, cf_b = 0, cf_c = 0;
     printf("Введите значения коэффициентов a, b и c в этом порядке.\n");
-    Input (&cf_a);
-    Input (&cf_b);
-    Input (&cf_c);
+    MainInput(&cf_a, &cf_b, &cf_c);
     double x1 = 0, x2 = 0;
     int Roots_count = EquationSolver (cf_a, cf_b, cf_c, &x1, &x2);
     Output(x1, x2, Roots_count);
-    // printf(RunAllTests(1, 1, 0, -4, -2, 2, 2));
+    printf("# RunAllTests: Test 1: %d", RunAllTests(1, 1, 0, -4, -2, 2, 2));
     return 0;
 }
 
@@ -36,6 +35,16 @@ int compare_double (double first, double second)
         return 0;
 }
 
+void MainInput (double* cf_a, double* cf_b, double* cf_c)
+{
+    printf("Коэффициент a: ");
+    Input(cf_a);
+    printf("Коэффициент b: ");
+    Input(cf_b);
+    printf("Коэффийиент c: ");
+    Input(cf_c);
+}
+
 void Input (double* cf)
 {
     int ch = 0;
@@ -43,7 +52,6 @@ void Input (double* cf)
     while (scanf("%lf", cf) != 1 || (ch = getchar()) != '\n')
     {
         while ((ch = getchar()) != '\n'){ ; }
-
         printf("Введены некорректные данные.\nВведите значение коэффициента заново: ");
     }
 }
@@ -66,8 +74,10 @@ int Linear (double b, double c, double* x1)
     else if (b_is_0 && !c_is_0)
         return 0;
     else
+    {
         * x1 = -c / b;
         return 1;
+    }
 }
 
 int Square (double a, double b, double c, double* x1, double* x2)
@@ -115,19 +125,15 @@ void Output (double x1, double x2, int Roots_count)
 
 
 // Юнит-тестирование
-/*
+
 int RunAllTests (int Test_count, double a, double b, double c, double x1_ideal, double x2_ideal, int Roots_count_ideal)
 {
     double x1, x2;
     int Roots_count = EquationSolver(a, b, c, &x1, &x2);
-    if (Roots_count != Roots_count_ideal || x1 != x1_ideal; || x2 != x2_ideal)
-    {
+    if (Roots_count != Roots_count_ideal || !compare_double(x1, x1_ideal) || !compare_double(x1, x1_ideal))
         printf("# ERROR Test %d, a = %lg, b = %lg, c = %lg, Roots_count = %d, x1 = %lg, x2 = %lg\n"
-               "Expected: x1 = %lg, x2 = %lg, Roots_count = %lg\n",
-               Test_count, a, b, c, Roots_count, x1, x2,
-               x1_ideal, x2_ideal, Roots_count_ideal);
-    }
+               "Expected: x1 = %lg, x2 = %lg, Roots_count = %d\n",
+                Test_count, a, b, c, Roots_count, x1, x2,
+                x1_ideal, x2_ideal, Roots_count_ideal);
     return Test_count;
 }
-
-*/
